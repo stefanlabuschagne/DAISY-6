d8df5671a8de8a2b92ca84a0408359a2af4f29e3
Stefan <stefan.labuschagne@gmail.com>
2020-01-13 13:14:56 +0200

    DaisyCase ViewModel

--- Models/ViewModels/DaisyCase.cs
+++ Models/ViewModels/DaisyCase.cs
@@ -7,13 +7,98 @@ namespace DAISY_6.Models.ViewModels
 {
     public class DaisyCase
     {
+        public string Name;
+        public string Surname;
+        public  string   Age ;
+        public  string   Sex ;
+        public  string   Race ;
+        public  string   Occupation ;
+        public  string   MedicalAid ;
+        public  string   Medication ;
+        public  string   Address ;
+        public  string   City ;
+        public  string   Province ;
+        public  string   PostalCode ;
+        public  string   Telephone1 ;
+        public  string   Telephone2 ;
+        public  string   EmailAddress ;
+        public  string   Suburb ;
+        public  string   DLLocation ;
+        public  string   DLType ;
+        public  string   DLSource ;
+        public  string   DLStudentNumber ;
+        public  string   Counsellor  ;
+        public  string   CallDate;
 
+        public Boolean ReasonADHD;
 
-        string Name;
-        string Surname;
+        public Boolean  ReasonAlzheimersDementia ;
+        public Boolean  ReasonAnxiety ;
+        public Boolean  ReasonBipolar ;
+        public Boolean  ReasonDebtPovertyUnemployment ;
+        public Boolean  ReasonDepression ;
+        public Boolean  ReasonEatingDisoirder ;
+        public Boolean  ReasonHIVAIDS ;
+        public Boolean  ReasonInformation ;
+        public Boolean  ReasonLossGriefLoneliness ;
+        public Boolean  ReasonNone ;
+        public Boolean  ReasonOCD ;
+        public Boolean  ReasonOtherIllness ;
+        public Boolean  ReasonPhysicalEmotionalAbuse ;
+        public Boolean  ReasonPostNatalDepression ;
+        public Boolean  ReasonPTSDTrauma ;
+        public Boolean  ReasonReationshipDomesticAbuse ;
+        public Boolean  ReasonSchizophrenia ;
+        public Boolean  ReasonSelfMutilation ;
+        public Boolean  ReasonSexualAbuseRape ;
+        public Boolean  ReasonSleepingDisorder ;
+        public Boolean  ReasonSocialPhobia ;
+        public Boolean  ReasonStress ;
+        public Boolean  ReasonSubstanceAbuse ;
+        public Boolean  ReasonSuicide ;
+        public Boolean  ReferDASGFaceToFace ;
+        public Boolean  ReferGP ;
+        public Boolean  ReferGovHospital ;
+        public Boolean  ReferLiteratureWebsite ;
+        public Boolean  ReferNone ;
+        public Boolean  ReferOther ;
+        public Boolean  ReferOtherNGOHelpline ;
+        public Boolean  ReferPoliceStation ;
+        public Boolean  ReferPrivateHospital ;
+        public Boolean  ReferPsychiatrist ;
+        public Boolean  ReferPsychologist ;
+        public Boolean  ReferRapeCrisis ;
+        public Boolean  ReferShelter ;
+        public Boolean  ReferSocialWorker ;
+        public Boolean  ReferSupportGroup ;
+        public Boolean  ReferTraumaCentre ;
 
+        public string CallSummary;
 
-
+        public Boolean  ReasonFrequentCaller ;
+        public Boolean  ReasonBullying ;
+        public Boolean  ReasonPregnancy ;
+        public Boolean  ReasonSexuality ;
+        public Boolean  ReferRehabilitationCentre ;
+        public Boolean  ReasonBorderlinePersonalityDisorder ;
+        public string   HelpLine;
+        public Boolean  ReasonOtherReason ;
+        public Boolean  ReasonFollowUp ;
+        public Boolean  ReferUniversityStudentWellness ;
+        public Boolean  ReferSadagHelpLine ;
+        public Boolean  ReferSelfHelp ;
+        public Boolean  ReferUber ;
+        public string   DLCallTimeOfDay;
+        public string   CalledOnBehalfOf;
+        public string ActionPoints;
+        public Boolean  ReasonRelationshipRomanticIssues ;
+        public Boolean  ReasonRelationshipFamilyIssues ;
+        public Boolean  ReferCompanyEAP ;
+        public Boolean  ReferUniversitySupportServices ;
+        public int   DistressRatingBegining ;
+        public int   DistressRatingEnd ;
+        public Boolean  ReasonAcademicStress ;
 
     }
+
 }
