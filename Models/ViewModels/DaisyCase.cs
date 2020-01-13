c5b51396214d4a562782b83efbadb2ba53d26606
Stefan <stefan.labuschagne@gmail.com>
2020-01-13 16:04:24 +0200

    Done

--- Models/ViewModels/DaisyCase.cs
+++ Models/ViewModels/DaisyCase.cs
@@ -20,6 +20,9 @@ namespace DAISY_6.Models.ViewModels
         [Required, Display(Name = "Called Date")]
         public string CallDate { get; set; }
 
+
+
+
         [Required, Display(Name = "First Name")]
         public string Name { get; set; }
         [Required, Display(Name = "Surname")]
@@ -51,7 +54,7 @@ namespace DAISY_6.Models.ViewModels
         public string Telephone1 { get; set; }
         [Required, Display(Name = "Telephone 2")]
         public string Telephone2 { get; set; }
-        [Required, Display(Name = "Telephone 1")]
+        [Required, Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
 
         [Required, Display(Name = "Academic Stress")]
@@ -127,41 +130,68 @@ namespace DAISY_6.Models.ViewModels
 
     public class DaisyDedicatedLineCase
     {
-        [Required]
+
+        [Required, Display(Name = "Counsellor")]
+        public string Counsellor { get; set; }
+        [Required, Display(Name = "Time of Day")]
+        public string DLCallTimeOfDay { get; set; }
+        [Required, Display(Name = "Helpline")]
+        public string HelpLine { get; set; }
+
+        [Required, Display(Name = "Called Date")]
+        public string CallDate { get; set; }
+
+        [Required, Display(Name = "First Name")]
         public string Name { get; set; }
-        [Required]
+        [Required, Display(Name = "Surname")]
         public string Surname { get; set; }
+
+        [Required, Display(Name = "Age Group")]
         public string Age { get; set; }
+        [Required, Display(Name = "Gender")]
         public string Sex { get; set; }
+        [Required, Display(Name = "Race")]
         public string Race { get; set; }
+        [Required, Display(Name = "Occupation")]
         public string Occupation { get; set; }
+        [Required, Display(Name = "Medical Aid")]
         public string MedicalAid { get; set; }
+        [Required, Display(Name = "Medication")]
         public string Medication { get; set; }
+        [Required, Display(Name = "Street / Box")]
         public string Address { get; set; }
-        public string City { get; set; }
-        public string Province { get; set; }
-        public string PostalCode { get; set; }
-        public string Telephone1 { get; set; }
-        public string Telephone2 { get; set; }
-        public string EmailAddress { get; set; }
+        [Required, Display(Name = "Suburb")]
         public string Suburb { get; set; }
+        [Required, Display(Name = "City")]
+        public string City { get; set; }
+        [Required, Display(Name = "Province")]
+        public string Province { get; set; }
+        [Required, Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+        [Required, Display(Name = "Telephone 1")]
+        public string Telephone1 { get; set; }
+        [Required, Display(Name = "Telephone 2")]
+        public string Telephone2 { get; set; }
+        [Required, Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+
         public string DLLocation { get; set; }
         public string DLType { get; set; }
         public string DLSource { get; set; }
         public string DLStudentNumber { get; set; }
-        public string Counsellor { get; set; }
 
-        public string CallDate { get; set; }
-        public string DLCallTimeOfDay { get; set; }
-        public string CalledOnBehalfOf { get; set; }
-
-        public string HelpLine { get; set; }
 
         public Boolean ReasonAcademicStress { get; set; }
         public Boolean ReasonADHD { get; set; }
         public Boolean ReasonAlzheimersDementia { get; set; }
         public Boolean ReasonAnxiety { get; set; }
         public Boolean ReasonBipolar { get; set; }
+
+        public Boolean ReasonBorderlinePersonalityDisorder { get; set; }
+        public Boolean ReasonBullying { get; set; }
+
+
         public Boolean ReasonDebtPovertyUnemployment { get; set; }
         public Boolean ReasonDepression { get; set; }
         public Boolean ReasonEatingDisoirder { get; set; }
@@ -172,26 +202,25 @@ namespace DAISY_6.Models.ViewModels
         public Boolean ReasonOCD { get; set; }
         public Boolean ReasonOtherIllness { get; set; }
         public Boolean ReasonPhysicalEmotionalAbuse { get; set; }
+
+
         public Boolean ReasonPostNatalDepression { get; set; }
+        public Boolean ReasonPregnancy { get; set; }
         public Boolean ReasonPTSDTrauma { get; set; }
         public Boolean ReasonReationshipDomesticAbuse { get; set; }
         public Boolean ReasonSchizophrenia { get; set; }
         public Boolean ReasonSelfMutilation { get; set; }
         public Boolean ReasonSexualAbuseRape { get; set; }
+        public Boolean ReasonSexuality { get; set; }
         public Boolean ReasonSleepingDisorder { get; set; }
         public Boolean ReasonSocialPhobia { get; set; }
         public Boolean ReasonStress { get; set; }
         public Boolean ReasonSubstanceAbuse { get; set; }
         public Boolean ReasonSuicide { get; set; }
-
         public Boolean ReasonFrequentCaller { get; set; }
-        public Boolean ReasonBullying { get; set; }
-        public Boolean ReasonPregnancy { get; set; }
-        public Boolean ReasonSexuality { get; set; }
         public Boolean ReasonOtherReason { get; set; }
         public Boolean ReasonFollowUp { get; set; }
-        public Boolean ReasonRelationshipRomanticIssues { get; set; }
-        public Boolean ReasonRelationshipFamilyIssues { get; set; }
+
 
         public Boolean ReferDASGFaceToFace { get; set; }
         public Boolean ReferGP { get; set; }
@@ -204,17 +233,16 @@ namespace DAISY_6.Models.ViewModels
         public Boolean ReferPrivateHospital { get; set; }
         public Boolean ReferPsychiatrist { get; set; }
         public Boolean ReferPsychologist { get; set; }
+        public Boolean ReferUniversityStudentWellness { get; set; }
+
+
         public Boolean ReferRapeCrisis { get; set; }
+        public Boolean ReferRehabilitationCentre { get; set; }
         public Boolean ReferShelter { get; set; }
         public Boolean ReferSocialWorker { get; set; }
         public Boolean ReferSupportGroup { get; set; }
         public Boolean ReferTraumaCentre { get; set; }
-        public Boolean ReferRehabilitationCentre { get; set; }
-        public Boolean ReasonBorderlinePersonalityDisorder { get; set; }
 
-        public Boolean ReferCompanyEAP { get; set; }
-        public Boolean ReferUniversitySupportServices { get; set; }
-        public Boolean ReferUniversityStudentWellness { get; set; }
         public Boolean ReferSadagHelpLine { get; set; }
         public Boolean ReferSelfHelp { get; set; }
         public Boolean ReferUber { get; set; }
@@ -222,10 +250,10 @@ namespace DAISY_6.Models.ViewModels
         public int DistressRatingBegining { get; set; }
         public int DistressRatingEnd { get; set; }
 
-        [Required, MinLength(100), MaxLength(8000)]
+        [Required, MinLength(100), MaxLength(8000), Display(Name = "Call Summary")]
         public string CallSummary { get; set; }
 
-        [Required, MinLength(100), MaxLength(8000)]
+        [Required, MinLength(100), MaxLength(8000), Display(Name = "Action Points")]
         public string ActionPoints { get; set; }
 
     }
